bae28222fab204802f391c4ab5d3ce997e55fc7f
AhmedEid3 <ahmed.eid3@outlook.com>
2023-09-08 15:58:59 +0300

    Add Higher Order  Design Pattern

---
+++ src/hoc/listing/Listings.tsx
@@ -0,0 +1,24 @@
+import withLoader from '../withLoader';
+import { IListing } from './IListing';
+import { Listing } from './Listing';
+import { ListingsGrid } from './ListingsGrid';
+
+interface IListingsProps {
+  data: {
+    listings: Array<IListing>;
+  };
+}
+
+export function Listings({ data }: IListingsProps) {
+  return (
+    <ListingsGrid>
+      {data.listings.map((listing) => (
+        <Listing key={listing.id} listing={listing} />
+      ))}
+    </ListingsGrid>
+  );
+}
+
+const ListingsWithLoader = withLoader(Listings, 'https://house-lydiahallie.vercel.app/api/listings');
+
+export default ListingsWithLoader;
---
+++ src/hoc/withLoader.tsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from 'react';
+import { LoadingSpinner } from '../components/LoadingSpinner';
+
+export default function withLoader(Element: any, url: string) {
+  return (props: any) => {
+    /* Add logic to:
+    1. Fetch data from the url that was passed as an argument.
+    2. Show the <LoadingSpinner /> while the  data is being fetched.
+    3. Pass the fetched data to the wrapped component.
+    */
+
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState<boolean>(true);
+
+    useEffect(() => {
+      setLoading(true);
+      fetch(url)
+        .then((res) => res.json())
+        .then((res) => setData(res))
+        .finally(() => setLoading(false));
+    }, []);
+
+    if (loading) return <LoadingSpinner />;
+
+    return <Element {...props} data={data} />;
+  };
+}
